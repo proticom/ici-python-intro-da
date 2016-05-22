a543d54560527bc9a9243ae9ac3d6af962baa2a8
proticom <etadros@proticom.com>
2016-05-22 02:11:05 -0700

    Added makeNumpyArray function

--- src/tadros_edward_week5.py
+++ src/tadros_edward_week5.py
@@ -11,21 +11,24 @@ def isNumpyArray(A):
     else:
         res = False
     return res
-    
+ 
+def makeNumpyArray(A): 
+    if isNumpyArray(A)==False:
+        A = np.array(A)
+    return A 
+
 def findSDArray(A):
     stdA = np.nanstd(A, axis=0)
     return stdA
 
 def normalizeArray(A):
-    if isNumpyArray(A)==False:
-        A = np.array(A)
+    A = makeNumpyArray(A)
     stdA = findSDArray(A)
     normA = A/stdA
     return normA
 
 def negativesToZero(A):
-    if isNumpyArray(A)==False:
-        A = np.array(A)
+    A = makeNumpyArray(A)
     A[np.nan_to_num(A)<0]=0
     return A
     
